eab846bba6a45b69ae6a1c2b75e7d23f1f2c8e35
Albertoo0 <160750395+Albertoo0@users.noreply.github.com>
2025-03-14 19:22:40 +0100

    Conexion to db calc_Info.py

--- calc_Info.py
+++ calc_Info.py
@@ -1,1 +1,30 @@
-#beta
+import sqlite3
+import pandas as pd
+from datetime import datetime
+
+def analizar_fraude_por_agrupaciones():
+    # Conexión a la base de datos
+    conn = sqlite3.connect('etl_database.db')
+    
+    # 1. Consulta para obtener datos de "Fraude" (tipo_incidencia = 5)
+    query = """
+        SELECT 
+            t.id_ticket,
+            t.cliente,
+            t.fecha_apertura,
+            t.tipo_incidencia,
+            e.id_emp,
+            e.nivel,
+            ce.tiempo
+        FROM tickets_emitidos t
+        JOIN contactos_con_empleados ce ON t.id_ticket = ce.id_ticket
+        JOIN empleados e ON ce.id_emp = e.id_emp
+        WHERE t.tipo_incidencia = '5'
+    """
+    df_fraude = pd.read_sql(query, conn)
+
+
+
+
+if __name__ == "__main__":
+    analizar_fraude_por_agrupaciones()
