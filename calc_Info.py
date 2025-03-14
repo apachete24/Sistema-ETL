c9cddf5e42e6bd2351f72c71f196465b329e8bcf
Albertoo0 <160750395+Albertoo0@users.noreply.github.com>
2025-03-14 20:13:36 +0100

    analisis, agrupaciones y resultados calc_Info.py

--- calc_Info.py
+++ calc_Info.py
@@ -24,6 +24,47 @@ def analizar_fraude_por_agrupaciones():
     df_fraude = pd.read_sql(query, conn)
 
 
+ if df_fraude.empty:
+        print("No hay incidentes")
+        return
+     
+#Calcular número de contactos por incidente     
+contactos_por_incidente = df_fraude.groupby('id_ticket').size().reset_index(name='num_contactos')
+
+#Extraer día de la semana
+df_fraude['fecha_apertura'] = pd.to_datetime(df_fraude['fecha_apertura'])
+df_fraude['dia_semana'] = df_fraude['fecha_apertura'].dt.day_name()
+
+
+#Función para análisis estadístico
+def analizar_agrupacion(df, grupo):
+        analisis = df.groupby(grupo).agg(
+            num_incidentes=('id_ticket', 'nunique'),
+            num_contactos=('id_ticket', 'size'),
+            media_contactos=('num_contactos', 'mean'),
+            mediana_contactos=('num_contactos', 'median'),
+            varianza_contactos=('num_contactos', 'var'),
+            max_contactos=('num_contactos', 'max'),
+            min_contactos=('num_contactos', 'min')
+        ).reset_index()
+        return analisis
+
+#agrupaciones
+agrupaciones = {
+        "Por empleado": ['id_emp'],
+        "Por nivel de empleado": ['nivel'],
+        "Por cliente": ['cliente'],
+        "Por tipo de incidente": ['tipo_incidencia'],
+        "Por día de la semana": ['dia_semana']
+    }
+#resultados
+resultados = {}
+    for nombre, grupo in agrupaciones.items():
+        resultados[nombre] = analizar_agrupacion(
+            df_fraude.merge(contactos_por_incidente, on='id_ticket'), 
+            grupo
+        )
+
 
 
 if __name__ == "__main__":
