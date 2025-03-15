66b7bd6947751952a4e00ce1e6a7e9d9d30bf439
apachete <ddaassmg@gmail.com>
2025-03-15 02:20:25 +0100

    pequeñas modificaciones

--- calculosDatos.py
+++ calculosDatos.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
-from BBDD import max_dias
-
-
-# CÁLCULOS CON PANDAS
+# EJERCICIO 1
 
 # 1. Número de muestras totales
 def getTotalMuestras(conn):
@@ -90,26 +87,4 @@ def getIncidentesEmpleado(conn):
     return min_incidentes_emp, max_incidentes_emp
 
 
-# Print resultados finales
-'''
-
-resultados = {
-    "Número de muestras": num_muestras,
-    "Media (valoración >=5)": round(media_valoracion, 2),
-    "Desviación (valoración >=5)": round(std_valoracion, 2),
-    "Media (incidentes/cliente)": round(media_incidentes, 2),
-    "Desviación (incidentes/cliente)": round(std_incidentes, 2),
-    "Media horas/incidente": round(media_horas_incidente, 2),
-    "Desviación horas/incidente": round(std_horas_incidente, 2),
-    "Mínimo horas/empleado": round(min_horas, 2),
-    "Máximo horas/empleado": round(max_horas, 2),
-    "Mínimo días resolución": min_dias,
-    "Máximo días resolución": max_dias,
-    "Mínimo incidentes/empleado": min_incidentes_emp,
-    "Máximo incidentes/empleado": max_incidentes_emp
-}
-
-print("Resultados:")
-for k, v in resultados.items():
-    print(f"- {k}: {v}")
-'''
+# EJERCICIO 2
